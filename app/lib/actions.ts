db0320061d5e084ec6cca3d8c52fd2fb6e4e295c
Valerii Viun <2455521@gmail.com>
2023-12-07 17:04:57 +0200

    added

---
+++ app/lib/actions.ts
@@ -0,0 +1,34 @@
+// Добавляя 'use server', вы помечаете все экспортированные функции в файле как функции сервера. Эти серверные функции затем можно импортировать в клиентские и серверные компоненты, что делает их чрезвычайно универсальными.
+'use server'
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+
+export async function createInvoice(formData: FormData) {
+ const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -24,7 +24,9 @@ export type Invoice = {
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
+  
 };
+
 
 export type Revenue = {
   month: string;
