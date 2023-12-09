bedbf46dea54a40193ef2150b059701af33b22dd
Valerii Viun <2455521@gmail.com>
2023-12-09 15:47:48 +0200

    fix

--- auth.ts
+++ auth.ts
@@ -6,6 +6,8 @@ import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
  
+
+// После проверки учетных данных создайте новую getUserфункцию, которая запрашивает пользователя из базы данных.
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
@@ -15,24 +17,27 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
+// Поставщик учетных данных позволяет пользователям входить в систему с использованием имени пользователя и пароля.
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [
+    providers: [
+    //   Добавление функции входа в систему
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
+//  запрашивает пользователя из базы данных.
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
             if (!user) return null;
+//  bcrypt.compareчтобы проверить, совпадают ли пароли:
             const passwordsMatch = await bcrypt.compare(password, user.password);
- 
           if (passwordsMatch) return user;
         }
- 
+        console.log('Invalid credentials');
         return null;
       },
     }),
